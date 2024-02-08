54ca41c097805a975dac6e7b7a403e9b4631e19b
Larissa de Sousa <larissa.sousa.lds@gmail.com>
2024-02-08 21:13:53 +0000

    controller

---
+++ backend/src/controllers/shop-list/shop-list.controller.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express()
+
+app.get('/shop-list', (req, res) => {
+
+res.send([])
+
+})
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const mysql = require('mysql2');
 const dbConfig = require('./config/mysql-config');
-
+const shopList = require('./controllers/shop-list/shop-list.controller');
 const app = express();
 
-app.get('/', (request, response) => response.status(200).send('Olá Mundo'));
+app.get('/', (req, res) => response.status(200).send('Olá Mundo'));
+
+const apiRouter = express.Router();
+
+app.use(express.json());
+apiRouter.use('/api', apiRouter);
+apiRouter.use('/shop-list ', shopList);
 
 app.listen(3333, () => console.log('Server run port 3333'));
