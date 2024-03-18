e72452321e33aa3a2b6fa45604e6616bce2e3fae
Larissa de Sousa <larissa.souas.lds@gmail.com>
2024-03-18 17:57:54 -0300

    update frontend

--- frontend/src/routes/ProtectedRoute/index.js
+++ frontend/src/routes/ProtectedRoute/index.js
@@ -4,8 +4,6 @@ import { Navigate, Outlet } from "react-router-dom";
 const ProtectedRoute = ({ children }) => {
 
   const authToken = Cookies.get("access_token");
-console.log("authToken", authToken)
-
     if (!authToken) {
       return <Navigate to="/login" replace />;
     }
