630064fbbe47436bde67965edc0ab8aa7f2f2b91
Larissa de Sousa <larissa.souas.lds@gmail.com>
2024-03-18 15:28:55 -0300

    deploy backend novamnete2

--- backend/src/server.js
+++ backend/src/server.js
@@ -16,7 +16,7 @@ const corsOptions = {
     optionsSuccessStatus: 200, 
   };
 
-app.use(cors(corsOptions));
+app.use(cors()));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
